f01403020d3706138fae3066850dc5477e3af1c8
vincent.sue <gnux123@gmail.com>
2016-03-15 18:01:41 +0800

    remove sass compiler.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var csswring = require('csswring');
+var precss = require('precss');
 var autoprefixer = require('autoprefixer-core');
 
 gulp.task('styles', function(){
 	var processors = [
+		precss(),
 		autoprefixer({browsers:['last 2 version']}),
 		csswring
 	];
 
-	return gulp.src('*.scss')
-				.pipe(sass())
+	return gulp.src('main.css')
 				.pipe(postcss(processors))
 				.pipe(gulp.dest('./css'));
 });
 
 gulp.task('watch:styles', function(){
-	gulp.watch('*.scss',['styles']);
+	gulp.watch('*.css',['styles']);
 });
